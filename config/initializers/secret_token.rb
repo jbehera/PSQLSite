33bb0c662f2db497304dd74ac09e948ee201ee70
Jay <jayachandra@proconstructor.com>
2011-10-10 12:29:42 +0530

    Initial commit comment...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PSQLSite::Application.config.secret_token = '6d3fc1adb98b4586104e536702a7b45dba3b2b4673b02addc50653819da87d1bc508f2a9c86e551840c4c2a3280c3c9a9f5df9ce6cb012be9e6444449410f63e'
